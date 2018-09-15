39c4bf6533b008fd01e3ae8b6202ab73d4e6b636
Nina Chen <nchen1@college.harvard.edu>
2018-09-15 14:44:07 -0400

    fron-end web styling finished

--- web/app.py
+++ web/app.py
@@ -1,6 +1,7 @@
 
 from flask import Flask, render_template, request, redirect
 from werkzeug import secure_filename
+from flask_bootstrap import Bootstrap
 import hashlib
 import cv2
 
@@ -17,18 +18,20 @@ def uploaded_file():
   if request.method == 'POST':
      f = request.files['file']
      video = f.filename
-     video_hash = str(int(hashlib.sha1(str(video).encode()).hexdigest(), 16) % (10 ** 8))
-     f.save(secure_filename(video))
-     select_value = request.form.get('select_value')
+     if video:
+        video_hash = str(int(hashlib.sha1(str(video).encode()).hexdigest(), 16) % (10 ** 8))
+        f.save(secure_filename(video))
+        select_value = request.form.get('select_value')
 
-     vidcap = cv2.VideoCapture(video)
+        vidcap = cv2.VideoCapture(video)
 
-     success,image = vidcap.read()
-     count = 0
-     while success:  
-         success,image = vidcap.read()
-         if count % 25 == 0:
-             cv2.imwrite("./static/" + video_hash + "frame%d.jpg" % int(count / 25), image)
+        success,image = vidcap.read()
+        count = 0
+        while success:  
+            success,image = vidcap.read()
+            if count % 25 == 0:
+                cv2.imwrite("./static/" + video_hash + "frame%d.jpg" % int(count / 25), image)
 
-         count += 1
-  return render_template('upload.html', video=video_hash, select_value=select_value, count=count)
+            count += 1
+        return render_template('upload.html', video=video_hash, select_value=select_value, count=count)
+  return render_template('index.html', error="Please Submit A File!")
